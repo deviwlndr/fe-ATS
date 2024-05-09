28e54934b3c53a271d53eb71d6274e853fe38253
deviwlndrr <wdevi443@gmail.com>
2024-05-09 22:59:21 +0700

    push

--- js/fetch.js
+++ js/fetch.js
@@ -3,18 +3,15 @@ import { addInner } from "https://bukulapak.github.io/element/process.js";
 import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
 import { isiTabel } from "./table.js";
 let urlAPI = "https://ws-ats-02a71362d079.herokuapp.com/undangan";
-get(urlAPI, isiTablePresensi);
-function isiTablePresensi(results) {
+get(urlAPI, GetAllRapatMakrab);
+function GetAllRapatMakrab(results) {
     results.forEach(isiRow);
 }
 function isiRow(value) {
     let content = 
-    isiTabel.replace("#NAMA#", value.biodata.nama)
-            .replace("#NOHP#", value.biodata.phone_number)
-            .replace("#JABATAN#", value.biodata.jabatan)
-            .replace("#LOKASI#", value.location)
-            .replace("#STATUS#", value.checkin)
-            .replace("#HARIKERJA#", value.biodata.hari_kerja)
+    isiTabel.replace("KEPADA", value.undangan_rapat.kepada)
+            .replace("ALAMAT", value.alamat)
+            .replace("UKM", value.ukm)
             .replace("#WARNA#", getRandomColor())
             .replace(/#WARNALOGO#/g, getRandomColorName());
         addInner("iniTabel", content);
